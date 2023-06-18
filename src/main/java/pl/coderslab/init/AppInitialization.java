7ee7050ada5db3389bff88921aa2bd25ff7bf78c
marcin <marcinludwikowski@op.pl>
2023-06-18 13:19:14 +0200

    configuration

---
+++ src/main/java/pl/coderslab/init/AppInitialization.java
@@ -0,0 +1,33 @@
+package pl.coderslab.init;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import pl.coderslab.app.AppConfig;
+
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class AppInitialization implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+
+        // create spring context
+        AnnotationConfigWebApplicationContext context =
+                new AnnotationConfigWebApplicationContext();
+
+        // configure spring context
+        context.register(AppConfig.class);
+        context.setServletContext(servletContext);
+
+        // create DispatcherServlet
+        ServletRegistration.Dynamic dispatcherServlet =
+                servletContext.addServlet("dispatcherServlet", new DispatcherServlet(context));
+
+        // configure DispatcherServlet
+        dispatcherServlet.setLoadOnStartup(1);
+        dispatcherServlet.addMapping("/");
+    }
+}
