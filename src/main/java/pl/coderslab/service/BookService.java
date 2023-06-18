5fee945651931e3a380197596beb1ab81f39eb77
marcin <marcinludwikowski@op.pl>
2023-06-18 17:51:58 +0200

    list all books

---
+++ src/main/java/pl/coderslab/service/BookService.java
@@ -0,0 +1,17 @@
+package pl.coderslab.service;
+
+import pl.coderslab.entity.Book;
+import java.util.List;
+import java.util.Optional;
+
+public interface BookService {
+    List<Book> getBooks();
+
+    Optional<Book> get(Long id);
+
+    void add(Book book);
+
+    void update(Book book);
+
+    void delete(Long id);
+}
